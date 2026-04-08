2c7931696df30bb238da742744078e54c95884a1
maryem chakerr <mariem.chaker@etudiant-fst.utm.tn>
2026-04-08 16:12:37 +0100

    without docker with n8n

--- fixtrack-frontend/vite.config.js
+++ fixtrack-frontend/vite.config.js
@@ -3,8 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    host: "0.0.0.0",
-    port: 4173,
+  server: {
+    host: "localhost",
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
   },
 });
